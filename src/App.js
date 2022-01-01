1778e895900ee64fa207041157a6749767a1e5ea
Richard Arthur <richardarthur228@gmail.com>
2022-01-01 17:28:45 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import "./App.css";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiSearch } from "react-icons/fi";
 
 function App() {
+  const searchRef = useRef(null);
+
+  console.log(searchRef.current);
+
+  const handleScroll = () => {
+    if (searchRef.current.getBoundingClientRect().bottom < 0) {
+      searchRef.current.classList.add("fixed-search");
+    }
+
+    if (window.pageYOffset === 0) {
+      searchRef.current.position = "relative";
+      searchRef.current.classList.remove("fixed-search");
+    }
+
+    console.log(window.pageYOffset);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <div className="header">
+        <div className="nav">
+          <AiOutlineMenu className="menu__icon" />
+          <img
+            className="logo"
+            src="https://s.zillowstatic.com/pfs/static/z-logo-white.svg"
+            alt=""
+          />
+          <p className="auth">Sign in</p>
+        </div>
+        <div className="hero__img"></div>
+        <div className="hero">
+          <h1 className="hero__title">Change starts here</h1>
+          <div className="hero__input" ref={searchRef}>
+            <input type="text" />
+            <FiSearch className="search__icon" />
+          </div>
+        </div>
+      </div>
+      <div className="main">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit aperiam
+          asperiores beatae quod enim aspernatur.
+        </p>
+      </div>
     </div>
   );
 }
